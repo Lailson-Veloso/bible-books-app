94c3d6727030c60f4e8fe8f2ed2327e10eb6c996
Lailson Veloso <lailson.veloso.c7@gmail.com>
2024-09-12 14:58:42 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, FlatList, TextInput } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+
 
 export default function App() {
+  const database = SQLite.openDatabaseSync('bible.db');
+
+  const [books, setBooks] = useState([]);
+  const [name, setName] = useState('');
+
+  const createTable = async () => {
+    try {
+        await database.execAsync(`
+            CREATE TABLE IF NOT EXISTS books(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            )
+        `)
+
+        console.log(database)
+    }
+        
+    catch (error) {
+        console.error(error)
+    }
+  }
+
+  const addBook = async () => {
+    try {
+        if (name == "") {
+            return alert("Campo nome obrigatório!")
+        }
+
+        await database.runAsync("INSERT INTO books (name) VALUES (?)", name)
+    }
+    
+    catch (error) {
+        console.error(error)
+    }
+  }
+
+  const readBooks = async () => {
+    try {
+      setBooks(await database.getAllAsync("SELECT * FROM books"))
+    }
+    
+    catch (error) {
+        console.error(error)
+    }
+  }
+
+  const dropTable = async () => {
+    try {
+        await database.execAsync('DROP TABLE IF EXISTS books')
+    }
+    
+    catch (error) {
+        console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    //createTable()
+  }, [createTable])
+
+  useEffect(() => {
+    readBooks()
+  }, [readBooks])
+
+  useEffect(() => {
+    //dropTable()
+  }, [dropTable])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', padding: 20 }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Bíblia - Livros</Text>
+      
+      <TextInput
+        placeholder="Nome do Livro"
+        onChangeText={setName}
+        style={{ borderBottomWidth: 1, marginBottom: 10 }}
+      />
+
+      <Button
+        title='Adicionar Livro'
+        onPress={() => addBook()}
+      />
+
+      <View>
+        <FlatList
+          data={books}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={{ marginVertical: 10 }}>
+              <Text>{item.name}</Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
